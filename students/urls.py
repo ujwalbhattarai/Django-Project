950a91fe74e4a7cb6eb7a293e6b62165b57aa1eb
Ujwal <bhattarai.ujwal20@gmail.com>
2026-09-18 23:46:36 +0545

    Added student details and form page and linked it to urls

--- students/urls.py
+++ students/urls.py
@@ -3,8 +3,30 @@ from . import views
 
 app_name = "students"
 
-
 urlpatterns = [
     path("", views.student_list, name="student-list"),
-    path("<int:student_id>/", views.student_detail, name="student-detail"),
+
+    path(
+        "<int:student_id>/",
+        views.student_detail,
+        name="student-detail"
+    ),
+
+    path(
+        "create/",
+        views.student_create,
+        name="student-create"
+    ),
+
+    path(
+        "<int:student_id>/edit/",
+        views.student_update,
+        name="student-update"
+    ),
+
+    path(
+        "<int:student_id>/delete/",
+        views.student_delete,
+        name="student-delete"
+    ),
 ]
--- students/views.py
+++ students/views.py
@@ -1,11 +1,15 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from django.urls import reverse
+from .models import Student
+
+
+from django.http import JsonResponse
+from django.shortcuts import render
+
 from .models import Student
 
 
 def student_list(request):
-
     students = Student.objects.all()
 
     return render(
@@ -14,12 +18,37 @@ def student_list(request):
         {"students": students}
     )
 
-def student_detail(request, student_id):
 
-    url = reverse("students:student-detail", args=[student_id])
+def student_detail(request, student_id):
+    student = Student.objects.get(id=student_id)
+
+    return render(
+        request,
+        "students/student_detail.html",
+        {"student": student}
+    )
+
+
+def student_create(request):
+    return render(
+        request,
+        "students/student_form.html"
+    )
+
+
+def student_update(request, student_id):
+    student = Student.objects.get(id=student_id)
+
+    return render(
+        request,
+        "students/student_form.html",
+        {"student": student}
+    )
+
+
+def student_delete(request, student_id):
+    student = Student.objects.get(id=student_id)
 
     return JsonResponse({
-        "id": student_id,
-        "url": url,
-        "Abhinay" : "Test"
+        "message": f"Delete student {student.name}"
     })
