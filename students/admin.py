77aeef3ff24d18cefb93ca5743fb52e9d4caa4d2
Ujwal <bhattarai.ujwal20@gmail.com>
2026-09-23 23:37:50 +0545

    Added filtering and display option to admin panels

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,107 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
-from .models import Student
+from .models import Student, Course, StudentProfile, Enrollment
 
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "age"]
+    search_fields = ["name"]
+    list_filter = ["age"]
+    ordering = ["name"]
+    fields = [
+        "name",
+        "age",
+    ]
 
-admin.site.register(Student)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name"]
+    ordering = ["name"]
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "student",
+        "address",
+        "phone",
+    ]
+
+    search_fields = [
+        "student__name",
+        "address",
+        "phone",
+    ]
+
+    fieldsets = (
+        (
+            "Student Information",
+            {
+                "fields": (
+                    "student",
+                )
+            },
+        ),
+        (
+            "Contact Information",
+            {
+                "fields": (
+                    "address",
+                    "phone",
+                )
+            },
+        ),
+    )
+
+def mark_as_inactive(modeladmin, request, queryset):
+    updated_count = queryset.update(
+        status="inactive"
+    )
+
+    messages.success(
+        request,
+        f"{updated_count} enrollment(s) marked as inactive."
+    )
+
+def mark_as_active(modeladmin, request, queryset):
+    updated_count = queryset.update(
+            status="active"
+    )
+
+    messages.success(
+        request,
+        f"{updated_count} enrollment(s) marked as active."
+    )
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "student",
+        "course",
+        "enrollment_date",
+        "status",
+    ]
+
+    search_fields = [
+        "student__name",
+        "course__name",
+    ]
+
+    list_filter = [
+        "status",
+        "course",
+        "enrollment_date",
+    ]
+
+    fields = [
+        "student",
+        "course",
+        "status",
+    ]
+
+    actions = [
+        mark_as_inactive,
+        mark_as_active,
+    ]
