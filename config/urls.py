2acf1682ad5024e7bc34d0092556650c118bd674
Ujwal <bhattarai.ujwal20@gmail.com>
2026-09-24 13:56:23 +0545

    Added registration form for the creation of users

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("students/", include("students.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
